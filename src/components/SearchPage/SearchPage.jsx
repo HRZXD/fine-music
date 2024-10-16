0b86617873616fff471ffbe1d05683eebb0ba5d8
Sakawai <139035944+Sakawai@users.noreply.github.com>
2024-10-16 18:20:43 +0700

    UPDATE

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -2,22 +2,33 @@ import React from "react";
 import "./searchpage.css";
 function SearchPage() {
   return (
-    <div className="Searchbar">
+    <div className="Main">
       <div className="Header">
         <a href="#Home">Home</a>
         <a href="Contact">Contact</a>
       </div>
-      <div className="Nav">
+      {/* Nav1 */}
+      <div className="Nav1">
         <a href="PopMusic">Pop Music</a>
         <a href="Metal">Metal</a>
         <a href="HipHop">Hip Hop</a>
         <a href="Jazz">Jazz</a>
       </div>
+      {/* Nav2 */}
+
+      {/* searchBars */}
       <form className="Search" action="">
         <div className="SearchBars">
-          <input type="text" placeholder="Search.." />
+          
+          <input type="text" placeholder="Search" />
         </div>
       </form>
+
+      <main className="Background">
+     
+      </main>
+      
+
     </div>
   );
 }
