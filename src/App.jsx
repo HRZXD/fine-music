a75a3c8212caba215cf2a2229c5243def8ecb51b
hrzdev <98503935+HRZXD@users.noreply.github.com>
2024-10-16 15:26:01 +0700

    UPDATE PAGE 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import SearchPage from './components/SearchPage/SearchPage'
+import SearchPage from './components/SearchPage/SearchPage.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import './searchpage.css'
+import React from "react";
+import "./searchpage.css";
 function SearchPage() {
   return (
-    <div>SearchPage</div>
-  )
+    <div className="Searchbar">
+      <div className="Header">
+        <a href="#Home">Home</a>
+        <a href="Contact">Contact</a>
+      </div>
+      <div className="Nav">
+        <a href="PopMusic">Pop Music</a>
+        <a href="Metal">Metal</a>
+        <a href="HipHop">Hip Hop</a>
+        <a href="Jazz">Jazz</a>
+      </div>
+      <form className="Search" action="">
+        <div className="SearchBars">
+          <input type="text" placeholder="Search.." />
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default SearchPage
+export default SearchPage;
