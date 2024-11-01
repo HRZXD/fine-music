c0e2a8a02112c38fa109ddb1e3a019a127368c1b
Sakawai <139035944+Sakawai@users.noreply.github.com>
2024-11-02 00:27:04 +0700

    Update
    FavBar

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -6,8 +6,9 @@ function SearchPage() {
   return (
     <div className="Main">
       <div className="Header">
-        <a href="#Home">Home</a>
+        <a href="Home">Home</a>
         <a href="Contact">Contact</a>
+        
       </div>
       {/* Nav1 */}
       <div className="Nav1">
@@ -21,18 +22,19 @@ function SearchPage() {
         <nav className="navbar-toggle">
           <input type="checkbox" href="#" class="menu-open" cheked/>
           <label class="menu-open-button" For="menu-open">
-            <span className="lines lines-1"></span>             
+            <span className="lines line-1"></span>             
+            <span className="lines line-2"></span>             
+            <span className="lines line-3"></span>             
           </label>
           
-          <a href="#" class="menu-item"><i class="" ></i></a>
+          <a href="#" class="menu-item" ></a>
         
         </nav>
+
       </header>
-              
-        
-              
 
-
+      
+              
       {/* searchBars */}
       <form className="Search" action="">
         <div className="SearchBars">
@@ -40,10 +42,12 @@ function SearchPage() {
           <input type="text" placeholder="Search" />
         </div>
       </form>
+        
+              
 
-      <main className="Background">
-     
-      </main>
+
+
+      
       
 
     </div>
