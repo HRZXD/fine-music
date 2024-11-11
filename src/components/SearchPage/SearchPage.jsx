7dafd2003cf5763509fa216f372dbbc9a89abf80
Sakawai <139035944+Sakawai@users.noreply.github.com>
2024-11-11 16:39:01 +0700

    Update

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -17,32 +17,53 @@ function SearchPage() {
             </div>
           </form>
         </div>
+    </header>
         <div className="Music-Data">
           <div className="Music-Box">
             <div className="Music-Seacrh">
               <div className="Head-Music">
                 <a href="">Music-Name</a>
               </div>
-              <div className="Music-detail">
-                <div className="Music-Thumnail">
-
-                </div>
+              <div className="Heart" >
+                <button className="Like-Heart" >❤</button>
+              </div>
+              <div className="Img">
+                <div className="Music-Thumnail"></div>
+              </div>
+              <div className="lyric">
+                <span>Lorem ipsum dolor sit amet consectetur, adipisicing,Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia alias laudantium recusandae asperiores  dignissimos non inventore!</span>
               </div>
             </div>
             <div className="Music-Seacrh">
               <div className="Head-Music">
                 <a href="">Music-Name</a>
               </div>
+              <div className="Heart" >
+                <button className="Like-Heart" >❤</button>
+              </div>
+              <div className="Img">
+                <div className="Music-Thumnail"></div>
+              </div>
+              <div className="lyric">
+                <span>Lorem ipsum dolor sit amet consectetur, adipisicing,Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia alias laudantium recusandae asperiores  dignissimos non inventore!</span>
+              </div>
             </div>
             <div className="Music-Seacrh">
               <div className="Head-Music">
                 <a href="">Music-Name</a>
               </div>
+              <div className="Heart" >
+                <button className="Like-Heart" >❤</button>
+              </div>
+              <div className="Img">
+                <div className="Music-Thumnail"></div>
+              </div>
+              <div className="lyric">
+                <span>Lorem ipsum dolor sit amet consectetur, adipisicing,Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia alias laudantium recusandae asperiores  dignissimos non inventore!</span>
+              </div>
             </div>
-            
           </div>
         </div>
-    </header>
     <footer className="Footer">
       <div className="footer" >
         <p>@FineMusicBy....</p>
